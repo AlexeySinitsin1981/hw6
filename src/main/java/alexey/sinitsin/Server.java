35f81f973004fe3d820ceef72def543a9ef11962
AlexeySinitsin1981 <20035@list.ru>
2019-11-13 13:32:52 +0300

    part 1

---
+++ src/main/java/alexey/sinitsin/Server.java
@@ -0,0 +1,43 @@
+package alexey.sinitsin;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        int port = 9999;
+
+        ServerSocket ss = new ServerSocket(port);
+        System.out.println("Waiting for a client...");
+        Socket socket = ss.accept();
+        System.out.println("Got a client!");
+
+        InputStream is = socket.getInputStream();
+        OutputStream ous = socket.getOutputStream();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(ous));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        while (!(line = br.readLine()).equals("")) {
+            sb.append(line);
+        }
+        if (sb.toString().contains("GET")) {
+            File file = new File("C:\\Users\\User\\IdeaProjects\\hw6");
+            if (file.isDirectory()) {
+                for (File item : file.listFiles()) {
+                    if (item.isDirectory()) {
+                        System.out.println(item.getName());
+                    }
+                }
+            }
+        }
+        System.out.println("The client just sent me this line: " + sb);
+        System.out.println("I'm sending it back...");
+        //bw.write(line);
+        //bw.flush();
+    }
+}
+
