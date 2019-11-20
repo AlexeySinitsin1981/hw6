b94101bd587e8e9b084f358baef6d0d1fe3be041
AlexeySinitsin1981 <20035@list.ru>
2019-11-20 15:35:35 +0300

    Исправлены замечания

--- src/main/java/alexey/sinitsin/Server.java
+++ src/main/java/alexey/sinitsin/Server.java
@@ -11,54 +11,60 @@ public class Server {
 
         ServerSocket ss = new ServerSocket(port);
         System.out.println("Waiting for a client...");
-        Socket socket = ss.accept();
-        System.out.println("Got a client!");
+        while (true) {
+            try(Socket socket = ss.accept()) {
+                System.out.println("Got a client!");
 
-        InputStream is = socket.getInputStream();
-        OutputStream ous = socket.getOutputStream();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(ous));
-        StringBuilder sb = new StringBuilder();
-        String line = null;
-        while (!(line = br.readLine()).equals("")) {
-            sb.append(line);
-        }
-        if (sb.toString().contains("GET")) {
-            File file = new File("C:\\Users\\User\\IdeaProjects\\hw6");
-            String body=new String();
 
-            if (file.isDirectory()) {
-                for (File item : file.listFiles()) {
-                    if (item.isDirectory()) {
-                        System.out.println(item.getName());
-                        body = body+" <li>"+item.getName()+" </li>\n";
+                try (BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))){
+                    StringBuilder sb = new StringBuilder();
+                    String line = null;
+                    while (!(line = br.readLine()).equals("")) {
+                        sb.append(line);
+                    }
+                    if (sb.toString().contains("GET")) {
+                        File file = new File(".");
+                        StringBuilder body = new StringBuilder();
+
+                        if (file.isDirectory()) {
+                            for (File item : file.listFiles()) {
+                                if (item.isDirectory()) {
+                                    System.out.println(item.getName());
+                                    body.append(" <li>").append(item.getName()).append(" </li>\n");
+
+                                }
+                            }
+                        }
+
+                        String bodySmall = new String(body.toString());
+                        body = new StringBuilder();
+                        body.append("<html><body><h1>Hello ").append("<ul>\n").append(bodySmall).append("</ul></h1></body></html>");
+                        StringBuilder header = new StringBuilder();
+                          header.append("HTTP/1.1 200 OK").append("\r\n").append("Server: YarServer/2009-09-09\r\n")
+                                  .append("Content-Type: text/html;charset=utf-8\r\n").append("Content-Length: ")
+                                  .append(body.length()).append("\r\n").append("Connection: close\r\n\r\n");
+
+                        bw.write(header.append(body.toString()).toString());
+                        bw.flush();
+                    } else {
+                        StringBuilder header = new StringBuilder();
+                        header.append("HTTP/1.1 404 Not Found").append("\r\n").append("Server: YarServer/2009-09-09\r\n")
+                                .append("Content-Type: text/html;charset=utf-8\r\n").append("Content-Length: ")
+                                .append(0).append("\r\n").append("Connection: close\r\n\r\n");
+
+                        bw.write(header.toString());
+                        bw.flush();
 
                     }
+
                 }
             }
-            body="<html><body><h1>Hello " +"<ul>\n"+body+"</ul></h1></body></html>";
-            String header = "HTTP/1.1 200 OK"+"\r\n" +
-                    "Server: YarServer/2009-09-09\r\n" +
-                    "Content-Type: text/html;charset=utf-8\r\n" +
-                    "Content-Length: " + body.length() + "\r\n" +
-                    "Connection: close\r\n\r\n";
-            bw.write(header+body);
-            bw.flush();
-        }else{
-            String header = "HTTP/1.1 404 Not Found" + "\r\n" +
-                    "Server: YarServer/2009-09-09\r\n" +
-                    "Content-Type: text/html;charset=utf-8\r\n" +
-                    "Content-Length: " + 0 + "\r\n" +
-                    "Connection: close\r\n\r\n";
-            bw.write(header);
-            bw.flush();
 
         }
 
     }
-
-
 
 }
 
